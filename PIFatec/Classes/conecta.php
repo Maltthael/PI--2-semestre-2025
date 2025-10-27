fd397cac297ac46862f6f03af44595aac22c0e8a
Rafael Cerqueira <149443159+rrafaelcerqueira@users.noreply.github.com>
2025-10-27 13:56:50 -0300

    Organizando Arquivos

---
+++ PIFatec/Classes/conecta.php
@@ -0,0 +1,36 @@
+<?php
+class conecta_bd {
+    private static $instance = null;
+    private $pdo;
+
+    // Construtor privado 
+    private function __construct() {
+        try {
+            $this->pdo = new PDO("mysql:host=localhost:3306;dbname=loja_informatica", "root", "");
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erro na conexão: " . $e->getMessage());
+        }
+    }
+
+    // Instância única
+
+    /* Evita múltiplas conexões desnecessárias ao banco.
+       Economiza recursos do servidor.
+       Mantém consistência em transações e consultas.
+       Facilita reutilizar o mesmo PDO em várias classes*/
+       
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new conecta_bd();
+        }
+        return self::$instance;
+    }
+
+    // Retorna a conexão PDO
+    public function getConnection() {
+        return $this->pdo;
+    }
+
+}
+?>
